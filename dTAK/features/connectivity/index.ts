b0961abd4c52cb299ad4ea999e68c28939011a31
Hunter M. <79799382+8tp@users.noreply.github.com>
2025-09-18 16:07:32 -0500

    Revert "Chat semi functional"

--- dTAK/features/connectivity/index.ts
+++
@@ -1,8 +0,0 @@
-export { ConnectivityProvider, useConnectivity } from "./ConnectivityProvider";
-export type { ConnectivityStatus } from "./ConnectivityProvider";
-export {
-	useMeshConnectivity,
-	subscribeToMeshConnectivity,
-	setMeshConnectivity,
-	getMeshConnectivity,
-} from "./meshConnectivity";
